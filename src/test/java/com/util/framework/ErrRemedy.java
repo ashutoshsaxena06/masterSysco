1878f035e4ee4a8a8d699bc042dc7c01dd34d2c0
Ashutosh esave <teamesave@gmail.com>
2019-11-07 08:51:15 +0530

    chnaged submit btn locator

---
+++ src/test/java/com/util/framework/ErrRemedy.java
@@ -0,0 +1,31 @@
+package com.util.framework;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
+public class ErrRemedy {
+
+	public static void ErrScreenshotCapture(WebDriver driver) {
+		// Take screenshot and store as a file format
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {
+			// now copy the screenshot to desired location using copyFile
+			// //method
+			FileUtils.copyFile(src, new File("C:\\Users\\ashsaxen\\Downloads\\errorScreenshotGFS\\error.png"));
+		} catch (Exception e) {
+			System.out.println("take screenshot failure");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void ErrReportingMail() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
